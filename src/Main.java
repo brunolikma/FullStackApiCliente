5d1577682b0f13b6fcdfd37ab16daaa0da0dc2cf
brunolikma <brunosssss@gmail.com>
2022-02-02 14:42:24 -0300

    Cliente com implementacao de validador do CPF

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Clientes bruno = new Clientes();
 
-        bruno.setCpf("3278SDFDFSDFSDFSDFSDDFSD563ASDASDASD78DASDASDASDSADADADASDAS60--------");
+        bruno.setCpf("327sdasdasdadasd387sdadasdasdasd356asdadasd60-d-ad-sad-ada-dad-a");
         bruno.setNomeCompleto("Bruno Lima de Santana");
         bruno.setTelefone("(13) 9 8173-1755");
 
